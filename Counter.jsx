6f391949e8c8f30d55b733ef87b6c2299912b148
Edward Rees <erees.hk@gmail.com>
2022-02-15 18:53:54 -0800

    Change of formatting

--- Counter.jsx
+++ Counter.jsx
@@ -4,18 +4,26 @@ import { View, Text, TouchableOpacity } from "react-native"; // React Native com
 // Stateless component
 const Counter = () => {
   const [counter, setCounter] = useState(0); // Allows for a simple state. Counter is the state, setCounter is the function to change the state
-  return ( // Actual components consolidated and returned
-    <View>
+  return (
+    // Actual components consolidated and returned
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+      }}
+    >
       {/* Button component, onPress handles action when pressed */}
       <TouchableOpacity onPress={() => setCounter(counter - 1)}>
         {/* Text within Button */}
-        <Text>-</Text>
+        <Text style={{ fontSize: 20, padding: 20 }}>-</Text>
       </TouchableOpacity>
       {/* Text of Counter */}
-      <Text>{counter}</Text>
+      <Text style={{ fontSize: 20 }}>{counter}</Text>
       {/* Button component, onPress handles action when pressed */}
       <TouchableOpacity onPress={() => setCounter(counter + 1)}>
-        <Text>+</Text>
+        <Text style={{ fontSize: 20, padding: 20 }}>+</Text>
       </TouchableOpacity>
       {/* Closing View tags */}
     </View>
