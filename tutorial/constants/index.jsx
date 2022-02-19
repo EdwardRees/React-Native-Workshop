8e2011684df5ba41e8303d1610a3be924a832242
Edward Rees <erees.hk@gmail.com>
2022-02-19 13:28:30 -0800

    Update of constants

--- tutorial/constants/index.jsx
+++ tutorial/constants/index.jsx
@@ -1,4 +1,4 @@
-const url = '10.0.0.158';
+const url = 'localhost'; // Change this to your server url, may help to run the ip.py file
 const port = "8080";
 
 const API = `http://${url}:${port}/api`; // localhost should actually be the IP address of your computer. This should technically be your public address. This is just an example.
